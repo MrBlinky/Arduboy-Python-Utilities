1577ae589921805c628c5b2853fc759be362e515
Mr.Blinky <4971163+MrBlinky@users.noreply.github.com>
2020-06-07 16:38:06 +0200

    Update Python 3.x.x support

--- sketch-backup.py
+++ sketch-backup.py
@@ -1,4 +1,5 @@
-print "\nArduboy sketch backup v1.0 by Mr.Blinky April 2018"
+#!/usr/bin/env python3
+print("\nArduboy sketch backup v1.11 by Mr.Blinky April 2018 Apr.2020")
 
 #requires pyserial to be installed. Use "pip install pyserial" on commandline
 
@@ -27,7 +28,6 @@ bootloader_active = False
 
 def delayedExit():
   time.sleep(2)
-  #raw_input()  
   sys.exit()
 
 def getComPort(verbose):
@@ -38,9 +38,9 @@ def getComPort(verbose):
       if  vidpid in device[2]:
         port=device[0]
         bootloader_active = (compatibledevices.index(vidpid) & 1) == 0
-        if verbose : print "Found {} at port {}".format(device[1],port)
+        if verbose : print("Found {} at port {}".format(device[1],port))
         return port
-  if verbose : print "Arduboy not found."
+  if verbose : print("Arduboy not found.")
 
 def bootloaderStart():
   global bootloader
@@ -48,9 +48,10 @@ def bootloaderStart():
   port = getComPort(True)
   if port is None : delayedExit()
   if not bootloader_active:
-    print "Selecting bootloader mode..."
+    print("Selecting bootloader mode...")
     bootloader = Serial(port,1200)
     bootloader.close()
+    time.sleep(0.5)
     #wait for disconnect and reconnect in bootloader mode
     while getComPort(False) == port :
       time.sleep(0.1)
@@ -63,22 +64,29 @@ def bootloaderStart():
   
 def bootloaderExit():
   global bootloader
-  bootloader.write("E")
+  bootloader.write(b"E")
   bootloader.read(1)
+  bootloader.close()
   
 ################################################################################
-  
+
+IncludeBootloader = "bootloader" in sys.argv[0]
+
 bootloaderStart()
 filename = time.strftime("sketch-backup-%Y%m%d-%H%M%S.bin", time.localtime())
-print "Reading sketch..."
-bootloader.write("A\x00\x00")
+print("Reading sketch...")
+bootloader.write(b"A\x00\x00")
 bootloader.read(1)
-bootloader.write("g\x70\x00F")
-backupdata = bytearray(bootloader.read(0x7000))
-print 'saving sketch to "{}"'.format(filename)
+if IncludeBootloader:
+  bootloader.write(b"g\x80\x00F")
+  backupdata = bytearray(bootloader.read(0x8000))
+else:
+  bootloader.write(b"g\x70\x00F")
+  backupdata = bytearray(bootloader.read(0x7000))
+print('saving sketch to "{}"'.format(filename))
 f = open (filename,"wb")
 f.write(backupdata)
 f.close
-print "Done"
+print("Done")
 bootloaderExit()
 delayedExit()
--- sketch-erase.py
+++ sketch-erase.py
@@ -1,9 +1,7 @@
-print "\nArduboy python sketch eraser v1.0 by Mr.Blinky May 2018"
+#!/usr/bin/env python3
+print("\nArduboy python sketch eraser v1.1 by Mr.Blinky May 2018 Jun.2020")
 
 #requires pyserial to be installed. Use "pip install pyserial" on commandline
-
-#rename this script filename to 'uploader-1309.py' to patch uploads on the fly
-#for use with SSD1309 displays
 
 import sys
 import time
@@ -27,17 +25,9 @@ compatibledevices = [
 ]
 
 bootloader_active = False
-caterina_overwrite = False
-
-flash_addr = 0
-flash_data = bytearray(chr(0xFF) * 32768)
-flash_page       = 1
-flash_page_count = 0
-flash_page_used  = [False] * 256
 
 def delayedExit():
-  time.sleep(3)
-  #raw_input()  
+  time.sleep(2)
   sys.exit()
 
 def getComPort(verbose):
@@ -48,9 +38,9 @@ def getComPort(verbose):
       if  vidpid in device[2]:
         port=device[0]
         bootloader_active = (compatibledevices.index(vidpid) & 1) == 0
-        if verbose : print "Found {} at port {}".format(device[1],port)
+        if verbose : print("Found {} at port {}".format(device[1],port))
         return port
-  if verbose : print "Arduboy not found."
+  if verbose : print("Arduboy not found.")
 
 def bootloaderStart():
   global bootloader
@@ -58,9 +48,10 @@ def bootloaderStart():
   port = getComPort(True)
   if port is None : delayedExit()
   if not bootloader_active:
-    print "Selecting bootloader mode..."
+    print("Selecting bootloader mode...")
     bootloader = Serial(port,1200)
     bootloader.close()
+    time.sleep(0.5)
     #wait for disconnect and reconnect in bootloader mode
     while getComPort(False) == port :
       time.sleep(0.1)
@@ -73,25 +64,26 @@ def bootloaderStart():
   
 def bootloaderExit():
   global bootloader
-  bootloader.write("E")
+  bootloader.write(b"E")
   bootloader.read(1)
+  bootloader.close()
   
 ################################################################################
 
 bootloaderStart()      
       
 ## Erase ##
-print "\nErasing sketch startup page"
-bootloader.write("A\x00\x00") #select page 0
+print("\nErasing sketch startup page")
+bootloader.write(b"A\x00\x00") #select page 0
 bootloader.read(1)
-bootloader.write("B\x00\x00F") #writing 0 length block will erase page only
+bootloader.write(b"B\x00\x00F") #writing 0 length block will erase page only
 bootloader.read(1)
-bootloader.write("A\x00\x00") #select page 0
+bootloader.write(b"A\x00\x00") #select page 0
 bootloader.read(1)
-bootloader.write("g\x00\x80F")# read 128 byte page
-if bytearray(bootloader.read(128)) == bytearray("\xff"* 128):
-    print "\nErase successful"
+bootloader.write(b"g\x00\x80F")# read 128 byte page
+if bytearray(bootloader.read(128)) == b"\xff" * 128:
+    print("\nErase successful")
 else :
-    print "\nErase failed"
+    print("\nErase failed")
 bootloaderExit()      
 delayedExit()
